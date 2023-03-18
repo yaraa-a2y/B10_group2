4c684a892642054977a000ea4ea0a6cdbd691838
Amera elsaadany <aabdulazizalsadani@stu.kau.edu.sa>
2023-03-18 14:11:50 +0300

    amera

--- src/b10_group2/B10_group2.java
+++ src/b10_group2/B10_group2.java
@@ -15,7 +15,7 @@ public class B10_group2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hi , i am amera ");
     }
     
 }
