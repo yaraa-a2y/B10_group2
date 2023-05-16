546f60c0e49d1977a93126a9ee07fea28ecaf21f
Amera elsaadany <aabdulazizalsadani@stu.kau.edu.sa>
2023-05-16 22:04:46 +0300

    amera

--- src/booking.java
+++ src/booking.java
@@ -7,4 +7,14 @@ private room room_type ;
 private int price_range ;
 private int num_of_people ;
 private int booking_number ;
+
+booking (int time ,String date ,room room_type ,int price_range ,int num_of_people ,int booking_number){
+    
+    this.time = time;
+    this.date = date;
+    this.room_type = room_type;
+    this.price_range = price_range;
+    this.num_of_people = num_of_people;
+    this.booking_number = booking_number;
+}
 }
