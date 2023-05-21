4c4d3d041d73dd31f94c69404f496110e202a480
Amera elsaadany <aabdulazizalsadani@stu.kau.edu.sa>
2023-05-21 21:03:10 +0300

    amera

--- src/reservation_system.java
+++ src/reservation_system.java
@@ -1,8 +1,51 @@
 
+import java.util.Scanner;
+
+
 public class reservation_system {
 
     
     public static void main(String[] args) {
+     
+ Scanner input = new Scanner(System.in);
+ 
+        System.out.println("----------------welcome to Stars Hotel -----------------------");
+        System.out.println("***************************************************************");
+        System.out.println("");
+        System.out.println("the services provided by our Hotel :- ");
+        System.out.println("****************************************************************");
+        System.out.println("1- browse rooms and suites  \n2- reservation    \n3- request a service    \n4- view list of reservation     \n5- exit  ");
+        System.out.println("please enter the number of service from the menu : ");
+        int num = input.nextInt();
+        
+        while(true){
+            if(num == 1){
+                
+                
+            }
+            if(num == 2){
+                
+                
+            }
+            if(num == 3){
+                
+                
+            }
+            if(num == 4){
+                
+                
+            }
+            if(num == 5){
+                break;
+                
+            }
+            
+            
+            
+        }
+        
+        
+        
         
     }
     
