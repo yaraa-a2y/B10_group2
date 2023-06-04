89505584ecdd45b99b74a488e2d34b60550b96ce
Yara_Alharbi <yreddatallaalharbi@stu.kau.edu.sa>
2023-06-05 01:13:04 +0300

    Yara commit

--- src/reservation_system.java
+++ src/reservation_system.java
@@ -51,7 +51,6 @@ public class reservation_system {
             }
             if (num == 3) {
                 System.out.println("Request a service :- \n-------------------------------------------------------------------\n");
-//                ser[f] = requestService();
                   DisplayService(services);
             }
             if (num == 4) {
@@ -65,10 +64,6 @@ public class reservation_system {
 
         }
 
-        booking b = null;
-        Scanner s = new Scanner(System.in);
-        reservation_system r = new reservation_system();
-//
 
         person arr[] = {
                 new customer("almurjan", 2014295, "SaraAhmad", 21, 0555557555, "female", "sa1ra", "123456789"),
@@ -128,30 +123,7 @@ public class reservation_system {
            System.out.println("Requested, thanks");    
        }
        
-       
-
-//    public static service requestService(){
-//        service[] services = {
-//                new service("Massage", 100),
-//                new service("Extra Bed", 50),
-//                new service("Breakfast", 50),
-//                new service("Parking", 30),
-//                new service("Spa", 150),
-//                new service("Flowers", 50),
-//
-//        };
-//        Scanner input = new Scanner(System.in);
-//        System.out.println("Enter the number of the service you want to request: ");
-//        for (int i = 0; i < services.length; i++) {
-//            System.out.println((i + 1) + "- " + services[i].getTypeOfService() + " " + services[i].getPrice() + " SR");
-//        }
-//        int num = input.nextInt();
-//        while (num < 1 || num > services.length) {
-//            System.out.println("Invalid number, please enter a valid number: ");
-//            num = input.nextInt();
-//        }
-//        return services[num - 1];
-//    }
+    
     public static customer cutomerInfo(booking []res,customer []cus,int f,service []ser){
         Scanner input = new Scanner(System.in);
 
@@ -225,77 +197,7 @@ public class reservation_system {
 
         System.out.println(res[f].toString());
      }
-//    public static void reservation(booking []res, customer []cus ,int f,service []ser) {
-//
-////        Scanner s = new Scanner(System.in);
-////
-////        System.out.println("Enter customer information:");
-////        System.out.println("Name:");
-////        String Name = s.next();
-////        System.out.println("id:");
-////        int id = s.nextInt();
-////        System.out.println("address:");
-////        String address = s.next();
-////        System.out.println("phone:");
-////        int Phone = s.nextInt();
-////        System.out.println("age:");
-////        int Age = s.nextInt();
-////        System.out.println("sex:");
-////        String Sex = s.next();
-////        System.out.println("user_name:");
-////        String user_name = s.next();
-////        System.out.println("pass_word:");
-////        String pass_word = s.next();
-////        cus[f]=new customer ( address, id, Name, Age, Phone, Sex, user_name, pass_word) ;
-//
-////        //Time
-////        System.out.print("The starting time for the booking day is:");
-////        int time = 14;
-////        System.out.println(time+"o'clock");
-////        //Date
-////        int room_num = (int)(Math.random()*100);
-////        System.out.println("Enter the entry date:");
-////        System.out.println("day:");
-////        int day = s.nextInt();
-////        System.out.println("month:");
-////        int month = s.nextInt();
-////        System.out.println("year:");
-////        int year = s.nextInt();
-////        Date date= new Date();
-////        SimpleDateFormat DateFormat=new SimpleDateFormat(day+"/"+month+"/"+year);
-////        String formattedDate = DateFormat.format(date);
-////        //Sitting days
-////        System.out.println("Enter the number of sitting days:");
-////        int days = s.nextInt();
-//        //Rooom type
-////        System.out.println("chose room type 1,2,3:");
-////        room roomArr []= {
-////                new room(1,"Studio with Sofa Bed","1 king bed " ,500,2)
-////                , new room(2,"Deluxe Suite","Bedroom 1: 1 king bed Bedroom 2: 1 twin bed Living room: 1 sofa bed ",1500,4)
-////                , new room(3,"King Suite","Bedroom 1: 1 king bed Bedroom 2: 1 king bed Bedroom 3: 1 twin bed  Living room: 2 sofa beds",5000,8)
-////        };
-////        for (int i = 0; i < roomArr.length; i++) {
-////            System.out.println(roomArr[i].toString());
-////        }
-////        int room_type = s.nextInt();
-////        //price
-////        int price = 0;
-////        for (int j = 0; j < roomArr.length; j++) {
-////            if(room_type==roomArr[j].getRoom_id()){
-////                price= roomArr[j].getRoom_price();
-////            }
-////        }
-////        int totalPrice= days*price;
-////        //Nomber of people
-////        System.out.println("Enter number of people:");
-////        int people = s.nextInt();
-//        //Booking Number
-////        booking b = new booking();
-////        //Booking
-////        
-////        res[f]= new booking(room_num ,time, formattedDate, days, room_type, totalPrice, people, b.getBooking_number(), Name , id,ser[f]);
-////        System.out.println(res[f].toString());
-//    }
+
 
 
     public static void ViewListOfReservation(booking [] res){
